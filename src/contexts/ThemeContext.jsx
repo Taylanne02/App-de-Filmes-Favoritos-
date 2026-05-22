72cdb9ae422e3a8ffa664234305fa0f16732c937
Laboratorios de Informatica CEULP <labins.pmw@ensino.ulbra.net>
2026-05-21 21:29:05 -0300

    Passo 3: Criando o ThemeContext

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -3,19 +3,19 @@ import { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("claro");
+  const [tema, setTema] = useState("claro");
 
   function alternarTema() {
-    setTheme((tema === "claro" ? "escuro" : "claro"));
+    setTema(tema === "claro" ? "escuro" : "claro");
   }
 
   useEffect(() => {
     document.body.className = tema;
   }, [tema]);
-  
+
   return (
     <ThemeContext.Provider value={{ tema, alternarTema }}>
       {children}
-    </ThemeContext.Provider>    
+    </ThemeContext.Provider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,14 @@
-import { ThemeContext, ThemeProvider } from "./contexts/ThemeContext";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-<ThemeProvider>
-  <App />
-</ThemeProvider>
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
